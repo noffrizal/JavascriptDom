94609afa3d173f04b2fc81a9517a7240c81719e3
noffrizal <zaimjr@gmail.com>
2022-07-04 08:52:40 +0700

    update some files event

---
+++ script3.js
@@ -0,0 +1,16 @@
+// const judul = document.getElementById('judul');
+// judul.innerHTML = "<em>Zaim</em>";
+
+// const sectionA  = document.querySelector('section#a');
+// sectionA.innerHTML = 'Hello';
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lighblue';
+// judul.style.backgroundColor = 'salmon';
+
+const judul = document.getElementsByTagName('h1')[0];
+judul.setAttribute('name', 'zaim');
+
+const a = document.querySelector('section#a a');
+a.setAttribute('id', 'link'); 
+
