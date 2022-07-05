6233421870389211e16e7d907c6c5f1692866cd5
noffrizal <zaimjr@gmail.com>
2022-07-05 14:51:41 +0700

    image js

--- traversal.js
+++ traversal.js
@@ -1,24 +1,43 @@
-// const close = document.querySelector('.close');
-// const card = document.querySelector('.card');
-// close.addEventListener('click', function () {
-//     card.style.display = 'none';
-// })
+// // const close = document.querySelector('.close');
+// // const card = document.querySelector('.card');
+// // close.addEventListener('click', function () {
+// //     card.style.display = 'none';
+// // })
 
-// Dom Traversal
-const close = document.querySelectorAll('.close');
-// for (let i = 0; i < close.length; i++) {
-//     close[i].addEventListener('click', function (e) {
-//     //    close[i].parentElement.style.display = 'none';
-//     e.target.parentElement.style.display = 'none';
-//     })
-// }
+// // Dom Traversal
+// const close = document.querySelectorAll('.close');
+// // for (let i = 0; i < close.length; i++) {
+// //     close[i].addEventListener('click', function (e) {
+// //     //    close[i].parentElement.style.display = 'none';
+// //     e.target.parentElement.style.display = 'none';
+// //     })
+// // }
 
-close.forEach(function(el) {
-    el.addEventListener('click', function (e) {
+// close.forEach(function(el) {
+//     el.addEventListener('click', function (e) {
+//         e.target.parentElement.style.display = 'none';
+//         // prevent default
+//         e.preventDefault();
+
+//         // bubling
+//         e.stopPropagation();
+//     });
+// });
+
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function (card) {
+//     card.addEventListener('click', function () {
+        
+//     });
+// });
+
+
+const container = document.querySelector('.container');
+
+container.addEventListener('click', function (e) {
+    if (e.target.className == 'close') {
         e.target.parentElement.style.display = 'none';
         e.preventDefault();
-    })
-});
-
-
+    }
+})
 
